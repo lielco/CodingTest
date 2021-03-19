ca96c3c6b222e22e53f8e8f2c9a82b13674089ef
LielCo <mslielcohen@gmail.com>
2021-03-19 19:51:16 +0200

    Fixed module structure, and made everything prettier.

---
+++ app/auth.py
@@ -0,0 +1,59 @@
+from flask import request, jsonify
+from jose import jwt
+import urllib
+from .auth_consts import AUTH0_DOMAIN, API_AUDIENCE, ALGORITHMS, JWKS_URL, ISSUER
+import json
+from functools import wraps
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+def get_token_from_auth_header():
+    """ Extracts the bearer token from the authorization header """
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise AuthError({"error" : "Missing authorization header"}, 401)
+    parts = auth.split()
+    if parts[0].casefold() != "bearer" or len(parts) != 2:
+        raise AuthError({"error" : "Invalid bearer token"}, 401)
+    token = parts[1]
+    return token
+
+def get_public_key_by_kid(kid):
+    """ Searches the JWKS for the key ID and returns the key.
+        If no key was found, returns null.
+    """
+    response = urllib.request.urlopen(JWKS_URL.format(AUTH0_DOMAIN))
+    pub_keys = json.load(response)
+    for key in pub_keys["keys"]:
+        if key["kid"] == kid:
+            return key
+    return None
+
+def token_required(f):
+    """ Checks JWT token existence and validity """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = get_token_from_auth_header()
+        try:
+            key_id = jwt.get_unverified_header(token).get('kid')
+            if not key_id:
+                raise AuthError({"error" : "No key ID"}, 401)
+            rsa_key = get_public_key_by_kid(key_id)
+            if not rsa_key:
+                raise AuthError({"error" : "Could not find specified key"}, 401)
+
+            payload = jwt.decode(
+                token, 
+                rsa_key, 
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer= ISSUER.format(AUTH0_DOMAIN),
+                options= {'verify_exp': False} # The given bearer token is expired
+        )
+        except jwt.JWTError as e:
+            raise AuthError({"message" : "Invalid token. error: {}".format(e)}, 401)
+        return f(*args,**kwargs)
+    return decorated
---
+++ app/auth_consts.py
@@ -0,0 +1,6 @@
+"""  """
+AUTH0_DOMAIN = "staging-netapp-cloud-account.auth0.com"
+API_AUDIENCE = "https://api.cloud.netapp.com"
+ALGORITHMS = ["RS256"]
+JWKS_URL = "https://{0}/.well-known/jwks.json"
+ISSUER = "https://{0}/"
